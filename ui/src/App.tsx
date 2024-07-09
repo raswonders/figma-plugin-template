6a4da3b5043155df56c246f3cf05f19540740bf4
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-07-09 18:13:24 +0200

    provide simple msg exchange example

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,26 @@
+import { useEffect } from "react";
+
 function App() {
-  return <h1>figma plugin template</h1>
+  const onClick = () => {
+    parent.postMessage({ pluginMessage: { type: "hello" } }, "*");
+  };
+
+  useEffect(() => {
+    const listener = (e: MessageEvent) => {
+      console.log("got this from plugin", e.data.pluginMessage);
+    };
+    window.addEventListener("message", listener);
+    return () => {
+      window.removeEventListener("message", listener);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>Figma plugin template</h1>
+      <button onClick={onClick}>say hello</button>
+    </>
+  );
 }
 
 export default App;
